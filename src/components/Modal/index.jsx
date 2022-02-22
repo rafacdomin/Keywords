ac2e5c36080f2a1e24f8138c52059a5cbb847df6
Rafael Domingues <rafacdomin@gmail.com>
2022-02-21 21:41:44 -0300

    Create modal wrapper component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,36 @@
+import React, { useCallback, useRef } from 'react'
+import PropTypes from 'prop-types'
+import { Wrapper } from './styles'
+import ReactDom from 'react-dom'
+
+const modalRoot = document.getElementById('modal-root')
+
+const Modal = ({ isOpen, controlModal, children }) => {
+	const modalRef = useRef(null)
+
+	const closeModal = useCallback(
+		event => {
+			if (modalRef.current === event.target) {
+				controlModal()
+			}
+		},
+		[controlModal],
+	)
+
+	return isOpen
+		? ReactDom.createPortal(
+				<Wrapper ref={modalRef} onClick={closeModal}>
+					{children}
+				</Wrapper>,
+				modalRoot,
+		  )
+		: null
+}
+
+Modal.propTypes = {
+	isOpen: PropTypes.bool.isRequired,
+	controlModal: PropTypes.func.isRequired,
+	children: PropTypes.node.isRequired,
+}
+
+export { Modal }
---
+++ src/components/Modal/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+	position: absolute;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	background: rgba(0, 0, 0, 0.3);
+
+	width: 100vw;
+	height: 100vh;
+	top: 0;
+	left: 0;
+	z-index: 10;
+`
